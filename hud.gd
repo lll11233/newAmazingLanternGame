8910a6936ec967a6de008b83f11223044ce182a4
lll11233 <laurenmzames@gmail.com>
2025-12-12 07:26:26 +1030

    small changes and edits

--- hud.gd
+++ hud.gd
@@ -2,14 +2,21 @@ extends CanvasLayer
 
 
 
-
 func _ready():
 	load_hearts()
 
 
 func load_hearts():
-	$HeartsFull.rect_size.x = Global.lives * 32
-	$HeartsEmpty.rect_size.x = (Global.max_lives - Global.lives)*32
-	$HeartsEmpty.rect_position.x = $HeartsFull.rect_position.x + $HeartsFull.rect_size.x * $HeartsFull.rect_scale.x
+	#$HeartsFull.size.x = Global.lives * 32
+	#$HeartsEmpty.size.x = (Global.max_lives - Global.lives)*32
+	#$HeartsEmpty.position.x = $HeartsFull.position.x + $HeartsFull.size.x * $HeartsFull.scale.x
+	#
+	###hearts HUD doesnt work, why????
+	var full = $HeartsFull
+	var empty = $HeartsEmpty
+	full.custom_minimum_size.x = Global.lives * 32
+	empty.custom_minimum_size.x = (Global.max_lives-Global.lives)*32
 	
-	##hearts HUD doesnt work, why????
+	empty.position.x = full.position.x + full.position.x + full.custom_minimum_size.x
+	
+	
--- portal_scene.gd
+++ portal_scene.gd
@@ -1,9 +1,10 @@
-extends CanvasLayer
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$portal_scene.hide()
+	#$portal_scene.hide()
+	pass
 
 
 
@@ -12,3 +13,6 @@ func _ready() -> void:
 func _on_portal_button_pressed() -> void:
 	pass # Replace with function body.
 	#change to level 2
+	$"scene 1/lava".hide()
+	$"scene 1/TextureRect2".hide()
+	$portal_scene.hide()
